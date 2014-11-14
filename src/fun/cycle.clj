19d8439eb00096b0d47c636c6a9431f9063b525a
Robert Levy <r.p.levy@gmail.com>
2014-11-14 02:31:49 -0500

    support other arities

--- src/fun/cycle.clj
+++ src/fun/cycle.clj
@@ -6,20 +6,25 @@
 (defn crotate "counterrotate" [coll]
   (cons (last coll) (butlast coll)))
 
-(defn funcycle [fns f]
+(defn funcycle [fns body-fn]
   (let [fns-atom (atom (crotate fns))
         rotor (fn [& args]
                 (swap! fns-atom rotate)
                 (apply (first @fns-atom) args))]
-    (f rotor)))
+    (body-fn rotor)))
 
-(defn reducycle [fns init coll]
+(defn reducycle
+  ([fns init coll]
+     (funcycle fns
+               (fn [rotor]
+                 (reduce rotor init coll))))
+  ([fns coll]
+     (funcycle fns
+               (fn [rotor]
+                 (reduce rotor coll)))))
+
+(defn mapcycle "almost rhymes with 'popsicle'" [fns & colls]
   (funcycle fns
             (fn [rotor]
-              (reduce rotor init coll))))
-
-(defn mapcycle "almost rhymes with 'popsicle'" [fns coll]
-  (funcycle fns
-            (fn [rotor]
-              (map rotor coll))))
+              (apply map rotor colls))))
 
--- test/fun/cycle_test.clj
+++ test/fun/cycle_test.clj
@@ -8,6 +8,8 @@
   (is (= [+ - * /] (rotate (crotate [+ - * /])))))
 
 (deftest reducycle-tests
+  (is (= 13 (reducycle [+ *] (range 1 5))))
+  (is (= 20 (reducycle [* +] (range 1 5))))
   (is (= 5 (reducycle [+ -] 0 (range 1 10))))
   (is (= -5 (reducycle [+ -] 0 (range 1 11))))
   (is (= 6 (reducycle [+ -] 0 (range 1 12))))
@@ -15,4 +17,10 @@
 
 (deftest mapcycle-tests
   (is (= ["1" 2 "3" 4] (mapcycle [str int] (range 1 5))))
-  (is (= ["a1" "b2" "a3" "b4"] (mapcycle [(partial str "a") (partial str "b")] (range 1 5)))))
+  (is (= ["a1" "b2" "a3" "b4"] (mapcycle [(partial str "a")
+                                          (partial str "b")]
+                                         (range 1 5))))
+  (is (= ["a12" "b23" "a34" "b45"] (mapcycle [(partial str "a")
+                                              (partial str "b")]
+                                             (range 1 5)
+                                             (range 2 6)))))
