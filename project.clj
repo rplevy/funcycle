b8b2b4f706b5eaabc2957db4bf47875b06114649
Robert Levy <r.p.levy@gmail.com>
2014-11-14 01:00:21 -0500

    change name to funcycle

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject reducycle "1.0.0"
-  :description "reducycle"
+(defproject funcycle "1.0.0"
+  :description "funcycle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
