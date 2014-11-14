b144a7900759654a5db28655de144794e2fd8b4a
Robert Levy <r.p.levy@gmail.com>
2014-11-14 00:30:55 -0500

    mapcycle too

--- src/red/ucycle.clj
+++ src/red/ucycle.clj
@@ -6,9 +6,20 @@
 (defn crotate "counterrotate" [coll]
   (cons (last coll) (butlast coll)))
 
-(defn reducycle [fns init coll]
+(defn funcycle [fns f]
   (let [fns-atom (atom (crotate fns))
         rotor (fn [& args]
                 (swap! fns-atom rotate)
                 (apply (first @fns-atom) args))]
-    (reduce rotor init coll)))
+    (f rotor)))
+
+(defn reducycle [fns init coll]
+  (funcycle fns
+            (fn [rotor]
+              (reduce rotor init coll))))
+
+(defn mapcycle "almost rhymes with 'popsicle'" [fns coll]
+  (funcycle fns
+            (fn [rotor]
+              (map rotor coll))))
+
--- test/red/ucycle_test.clj
+++ test/red/ucycle_test.clj
@@ -12,3 +12,7 @@
   (is (= -5 (reducycle [+ -] 0 (range 1 11))))
   (is (= 6 (reducycle [+ -] 0 (range 1 12))))
   (is (= -6 (reducycle [+ -] 0 (range 1 13)))))
+
+(deftest mapcycle-tests
+  (is (= ["1" 2 "3" 4] (mapcycle [str int] (range 1 5))))
+  (is (= ["a1" "b2" "a3" "b4"] (mapcycle [(partial str "a") (partial str "b")] (range 1 5)))))
